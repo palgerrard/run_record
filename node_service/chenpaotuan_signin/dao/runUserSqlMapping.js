c6caac4419a8cd2c23069a524529e13932a201fd
palgerrard <palgerrard@163.com>
2016-08-26 00:28:18 +0800

    rm all

--- node_service/chenpaotuan_signin/dao/runUserSqlMapping.js
+++
@@ -1,9 +0,0 @@
-// dao/runUserSqlMapping.js
-// CRUD SQL语句
-var runRecord = {
-    simpleInsert: 'insert into run_user(user_name, created, updated) VALUES(?,?,?)',
-    queryByName: 'select * from run_user where user_name=?',
-    queryAll: 'select * from run_user'
-};
-
-module.exports = runRecord;
