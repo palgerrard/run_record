6b39ea68a9360cbed00f9488ad0f8915dad86835
palgerrard <palgerrard@163.com>
2016-08-26 00:49:29 +0800

    Update db.js

--- conf/db.js
+++ conf/db.js
@@ -4,8 +4,8 @@ module.exports = {
     mysql: {
         host: 'localhost',
         user: 'root',
-        password: 'hellomysql123',
-        database: 'db_chenpaotuan', // 前面建的user表位于这个数据库中
+        password: '*保密*',
+        database: '*保密*', // 前面建的user表位于这个数据库中
         port: 3306
     }
 };
